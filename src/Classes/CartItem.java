14004fef385f301bd47e7ed2d5d0d50236613f81
Mahmoud Reda Sharaf Eldeen <141069809+Sharaf-Eldeen@users.noreply.github.com>
2024-11-03 23:36:48 +0200

    Create Car Item Class

---
+++ src/Classes/CartItem.java
@@ -0,0 +1,28 @@
+package Classes;
+
+public class CartItem {
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public double getTotalItemPrice() {
+        return product.getTotalPrice(quantity);
+    }
+
+    public boolean isShipple() {
+        return product instanceof ShippableProduct;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    
+}
